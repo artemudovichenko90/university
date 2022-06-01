8da8d794b7f8f37b610901b0ba616c25b70b6d6c
Artem Udovichenko <artemudovichenko90@gmail.com>
2022-06-02 01:08:26 +0300

    Add class Student

---
+++ assets/js/student.js
@@ -0,0 +1,32 @@
+class Student extends User {
+  /**
+   * Create student
+   * @param {string} firstName 
+   * @param {string} lastName 
+   * @param {Date} year 
+   */
+  constructor(firstName, lastName, year) {
+    super(firstName, lastName);
+    this.year = year;
+  }
+  /**
+   * Set date of admission to the university
+   * @param {Date} year 
+   */
+  set year(year) {
+    const now = new Date();
+    const limit = new Date().setFullYear(now.getFullYear() - 5);
+    console.log(now);
+    console.log(limit);
+    if (year > now || year < limit) {
+      throw new RangeError('Incorrect date')
+    }
+    this._year = year;
+  }
+  get year() {
+    return this._year;
+  }
+  get course() {
+    return new Date().getFullYear() - this.year.getFullYear();
+  }
+}
