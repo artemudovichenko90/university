486c0d708a461648c0e7e5e8e35901483c30000f
Artem Udovichenko <artemudovichenko90@gmail.com>
2022-06-02 01:08:46 +0300

    Add class Group

---
+++ assets/js/group.js
@@ -0,0 +1,24 @@
+class Group {
+  constructor(name, students) {
+    this.name = name;
+    this.students = students;
+  }
+  set name(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  set students(students) {
+    this._students = students;
+  }
+  get students() {
+    return this._students;
+  }
+  showStudents() {
+    this._students.forEach(v => console.log(this.#shortFullName(v)));
+  }
+  #shortFullName(student) {
+    return `${student.firstName} ${student.lastName[0]}.`;
+  }
+}
